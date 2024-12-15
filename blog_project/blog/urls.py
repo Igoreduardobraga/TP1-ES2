d17ce9a4f10f4278e65bc98090c935924d0d5ac6
igoreduardobraga <igoredubraga@gmail.com>
2024-12-14 23:21:32 -0300

    adicionando login e logout

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.home, name='home'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),  # Verifique esta linha
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('post_list/', views.post_list, name='post_list'),
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,18 +1,21 @@
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -26,6 +29,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -38,9 +42,21 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form, 'post': post})
 
 
+@login_required
 def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
         post.delete()
         return redirect('post_list')
     return render(request, 'blog/post_confirm_delete.html', {'post': post})
+
+
+@login_required
+def custom_logout(request):
+    logout(request)
+    return redirect('home')
+
+def home(request):
+    if request.user.is_authenticated:
+        return redirect('post_list')
+    return redirect('login')
