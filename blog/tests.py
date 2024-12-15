850d500c69133d927a6f6f0a323abb709f31f6f4
Alan Prado <alanctprado@gmail.com>
2024-12-15 18:51:17 -0300

    sanity check

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
-# Create your tests here.
+class AlwaysFailTest(TestCase):
+    def test_always_fails(self):
+        self.fail("This test is designed to always fail.")
