c8250e50e60c5f8472698ac58f71e6e66279ad90
Alan Prado <alanctprado@gmail.com>
2025-01-11 21:08:57 -0300

    basic e2e test and refactored test structure

---
+++ blog/tests/test_e2e_login.py
@@ -0,0 +1,17 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+class MyEndToEndTests(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.driver = webdriver.Chrome()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+        super().tearDownClass()
+
+    def test_example(self):
+        self.driver.get(f'{self.live_server_url}/')
+        self.assertIn('Login', self.driver.title)
